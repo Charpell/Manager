41300c1f45ee6f7626288330fa9431194d8833f7
Charpell <charpellumeh@gmail.com>
2018-05-17 11:01:33 +0100

    Display Multiple Screens

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Scene, Router, Stack } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 
 const RouterComponent = () => {
@@ -8,6 +9,7 @@ const RouterComponent = () => {
     <Router>
       <Stack key="root">
         <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene key="employeeList" component={EmployeeList} title="Employees" initial />
       </Stack>
     </Router>
   )
