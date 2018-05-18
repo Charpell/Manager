9b4623d45053afdbb20348b3234c820ddccf4525
Charpell <charpellumeh@gmail.com>
2018-05-18 03:01:59 +0100

    Styling the Confirm Component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,12 +3,16 @@ import { connect } from 'react-redux'
 import _ from 'lodash';
 import Communications from 'react-native-communications';
 
-import { Card, CardSection, Button } from './common'
+import { Card, CardSection, Button, Confirm } from './common'
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions'
 
 
 class EmployeeEdit extends Component {
+  state = {
+    showModal: false
+  }
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -42,6 +46,18 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     )
   }
